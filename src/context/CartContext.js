f42576c2a2c48170ee8440e43be30c366a105cc2
ShruthiBingi <bingishruthi369@gmail.com>
2022-01-23 15:27:38 +0000

    NxtTrendz App

---
+++ src/context/CartContext.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CartContext = React.createContext({
+  cartList: [],
+  removeAllCartItems: () => {},
+  addCartItem: () => {},
+  removeCartItem: () => {},
+  incrementCartItemQuantity: () => {},
+  decrementCartItemQuantity: () => {},
+  quantity: 1,
+})
+
+export default CartContext
